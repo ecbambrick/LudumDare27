60d5933dc865d4f8b3944fd39aa4c95162d2b886
Cole Bambrick <kcirbmab@gmail.com>
2013-08-23 23:03:10 -0400

    cleaned up main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,26 +1,14 @@
 function love.load()
 
-	secs = require "secs"
+	secs = require "lib.secs"
+		   require "systems.tiledmap"
 		   require "systems.input"
 		   require "systems.rendering"
-	
-	secs.component("input", {})
-	secs.component("physical", {})
-	secs.component("rect", { style = "fill", rgb = { 255,255,255,255 }})
-	secs.component("pos",  { x = 0, y = 0, z = 0, w = 0, h = 0, dx = 1 })
-	secs.component("vel",  { x = 0, y = 0, maxX = 100, maxY = 250 })
-	
-	secs.type("controllable", "input", "vel")
-	secs.type("rectangles", "rect", "pos")
-	secs.type("movable", "pos", "vel")
-	
-	secs.entity(
-		{ "pos", { x = 10, y = 10, w = 16, h = 16 }},
-		{ "physical" },
-		{ "input" },
-		{ "rect" },
-		{ "vel" }
-	)
+		   require "components"
+		   require "entities"
+		   
+	Player = secs.entity.player(10, 10)
+	Map = secs.entity.stage("assets/test.tmx", true)
 	
 end
 
